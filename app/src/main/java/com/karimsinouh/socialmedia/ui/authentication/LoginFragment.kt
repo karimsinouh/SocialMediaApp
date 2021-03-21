4c40e8bc2014df3579b55054ae8c1d5a47dd49a7
karim <sinouhkar@gmail.com>
2021-03-21 14:02:31 -0700

    add firebase dependencies

--- app/src/main/java/com/karimsinouh/socialmedia/ui/authentication/LoginFragment.kt
+++ app/src/main/java/com/karimsinouh/socialmedia/ui/authentication/LoginFragment.kt
@@ -1,6 +1,36 @@
 package com.karimsinouh.socialmedia.ui.authentication
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.karimsinouh.socialmedia.R
+import com.karimsinouh.socialmedia.databinding.FragmentLoginBinding
 
-class LoginFragment:Fragment() {
+class LoginFragment:Fragment(R.layout.fragment_login) {
+
+    private lateinit var binding:FragmentLoginBinding
+    private lateinit var nav:NavController
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding= FragmentLoginBinding.bind(view)
+        nav=findNavController()
+
+
+        binding.signup.setOnClickListener {
+            navigateToSignUp()
+        }
+
+        binding.login.setOnClickListener {
+            navigateHome()
+        }
+
+    }
+
+    private fun navigateToSignUp()=nav.navigate(R.id.login_to_signUpFragment)
+    private fun navigateHome()=nav.navigate(R.id.login_to_signUpFragment)
+
+
 }
