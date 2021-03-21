8876865e9c5507cc54ff9651f9a69e52ceba2124
karim <sinouhkar@gmail.com>
2021-03-21 10:25:35 -0700

    setup dependency injection

---
+++ app/src/main/java/com/karimsinouh/socialmedia/App.kt
@@ -0,0 +1,7 @@
+package com.karimsinouh.socialmedia
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App:Application()
---
+++ app/src/main/java/com/karimsinouh/socialmedia/di/SingletonsModule.kt
@@ -0,0 +1,25 @@
+package com.karimsinouh.socialmedia.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SingletonsModule {
+
+
+    @Provides
+    @Singleton
+    fun glide(@ApplicationContext c:Context)=Glide.with(c).applyDefaultRequestOptions(RequestOptions())
+
+
+
+}
